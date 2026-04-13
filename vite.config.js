c8e0a6112b4da8b5a19ae501adf68dc130bc7474
sidd-j <53267391+sidd-j@users.noreply.github.com>
2026-04-13 11:59:52 +0530

    GIt FIXES 2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/SS_Portfolio/'   // 👈 MUST match repo name EXACTLY
+  base: '/DevCraft/'   // 
 })
